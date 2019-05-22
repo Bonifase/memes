0b3ff537498d487c604a14694d0490d74be4f6f8
Bonifase <orwabonifase@gmail.com>
2019-05-22 23:01:18 +0300

    first page setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    selectedFile: null,
+    headerText: "",
+    footerText: "",
+    Author: ""
+  };
+
+  fileChangedHandler = event => {
+    this.setState({
+      selectedFile: URL.createObjectURL(event.target.files[0]),
+      headerText: event.target.header,
+      footerText: event.target.footer,
+      author: event.target.author
+    });
+  };
+  generateMeme = event => {
+    this.setState({
+      headerText: event.target.header,
+      footerText: event.target.footer,
+      author: event.target.author
+    });
+    console.log(this.state);
+  };
+  render() {
+    return (
+      <div className="App-header">
+        <header className="App-logo">
+          {this.state.selectedFile === null ? (
+            <input type="file" onChange={this.fileChangedHandler} />
+          ) : (
+            <img src={this.state.selectedFile} />
+          )}
+          <form>
+            <input type="text" />
+            <br />
+            <input type="text" />
+            <br />
+            <input type="text" />
+            <button onClick={this.generateMeme}>Generate!</button>
+          </form>
+        </header>
+      </div>
+    );
+  }
 }
-
-export default App;
