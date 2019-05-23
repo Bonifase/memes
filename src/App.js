b7c37a4188b302350ecdd44eaf9697ace633d04d
Bonifase <orwabonifase@gmail.com>
2019-05-23 07:39:47 +0300

    styling with css

--- src/App.js
+++ src/App.js
@@ -1,46 +1,72 @@
 import React, { Component } from "react";
+import Image from "./components/Image";
 import "./App.css";
 
 export default class App extends Component {
   state = {
+    data: {
+      headerText: "",
+      footerText: "",
+      Author: ""
+    },
     selectedFile: null,
-    headerText: "",
-    footerText: "",
-    Author: ""
+    loading: false
   };
 
-  fileChangedHandler = event => {
+  fileChangedHandler = e => {
+    e.preventDefault();
     this.setState({
-      selectedFile: URL.createObjectURL(event.target.files[0]),
-      headerText: event.target.header,
-      footerText: event.target.footer,
-      author: event.target.author
+      selectedFile: URL.createObjectURL(e.target.files[0])
     });
   };
-  generateMeme = event => {
+  onSubmit = e => {
+    e.preventDefault();
     this.setState({
-      headerText: event.target.header,
-      footerText: event.target.footer,
-      author: event.target.author
+      loading: true
     });
     console.log(this.state);
   };
+  onChange = e => {
+    this.setState({
+      ...this.state,
+      data: { ...this.state.data, [e.target.name]: e.target.value }
+    });
+    console.log(this.state);
+  };
+
   render() {
+    let data = this.state.data;
     return (
-      <div className="App-header">
+      <div className="showcasew">
         <header className="App-logo">
           {this.state.selectedFile === null ? (
             <input type="file" onChange={this.fileChangedHandler} />
           ) : (
-            <img src={this.state.selectedFile} />
+            <p className="container">
+              <Image props={this.state} />
+            </p>
           )}
-          <form>
-            <input type="text" />
+          <form onSubmit={this.onSubmit} loading={this.state.loading}>
+            <input
+              type="text"
+              id="header"
+              name="header"
+              placeholder="Header Text"
+              value={data.header}
+              onChange={this.onChange}
+            />
             <br />
-            <input type="text" />
+            <input
+              type="text"
+              id="footer"
+              name="footer"
+              placeholder="Footer Text"
+              value={data.footer}
+              onChange={this.onChange}
+            />
             <br />
-            <input type="text" />
-            <button onClick={this.generateMeme}>Generate!</button>
+            <input type="text" id="author" name="author" placeholder="You Name" value={data.author} onChange={this.onChange} />
+            <button>Generate!</button>
           </form>
         </header>
       </div>
--- src/components/Image.js
+++ src/components/Image.js
@@ -1,5 +1,13 @@
 import React from "react";
 
 export default function Image(props) {
-  return <div />;
+  console.log("hhhijjio", props.props);
+  return (
+    <div className="containe">
+      <img className="image" src={props.props.selectedFile} alt="Snow" />
+      <h2 className="top">{props.props.data.header}</h2>
+      <h2 className="bottom">{props.props.data.footer}</h2>
+      <h2 className="center">{props.props.data.Author}</h2>
+    </div>
+  );
 }
